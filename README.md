23c084000fbe9a872382694e879b722888ddb708
Loren 🤓 <lorensr@gmail.com>
2023-04-12 11:29:56 -0700

    Update README

--- README.md
+++ README.md
@@ -1,8 +1,51 @@
-Sync an organization's GitHub repo list to a Notion DB
+Sync an organization's GitHub repo list and permissions to a Notion DB
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+- [Development](#development)
+  - [Setup](#setup)
+  - [Running](#running)
+- [Deployment](#deployment)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
 ## Development
 
+### Setup
+
 ```sh
-NOTION_TOKEN="foo" GITHUB_TOKEN="bar" deno task start
-GITHUB_TOKEN="bar" USER="baz" deno task check-rate-limit
+git clone https://github.com/temporalio/github-repo-notion-sync.git
+cd github-repo-notion-sync
+npm i
+```
+
+### Running
+
+```
+temporal server start-dev
+```
+
+```
+NOTION_TOKEN="foo" GITHUB_TOKEN="bar" npm start
+```
+
+If you need to check how many calls to the GitHub API you have left, run:
+
+```sh
+GITHUB_TOKEN="bar" USER="github_username" npm run check-rate-limit
+```
+
+## Deployment
+
+- Get a [Cloud account](https://temporal.io/cloud) 
+- Deploy this code somewhere that supports long-running Node processes
+- Set these env vars:
+
+```
+NOTION_TOKEN="<notion integration token>"
+GITHUB_TOKEN="<personal access token with org read permissions>"
+NODE_ENV="production"
+TEMPORAL_CLOUD_CERT="-----BEGIN CERTIFICATE----- EUsd..."
+TEMPORAL_CLOUD_KEY="-----BEGIN PRIVATE KEY----- LQaz..."
 ```
--- check-rate-limit.ts
+++ check-rate-limit.ts
@@ -1,23 +1,29 @@
-// https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#checking-your-rate-limit-status
-const response = await fetch(
-  `https://api.github.com/users/${process.env.USER}`,
-  {
-    headers: {
-      Accept: 'application/vnd.github+json',
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  }
-)
+import fetch from 'node-fetch'
 
-const limit = response.headers.get('x-ratelimit-limit')
-const remaining = response.headers.get('x-ratelimit-remaining')
-const used = response.headers.get('x-ratelimit-used')
-const reset = response.headers.get('x-ratelimit-reset')
-console.log('limit:', limit)
-console.log('remaining:', remaining)
-console.log('used:', used)
-console.log(
-  'reset:',
-  new Date(parseInt(reset as string) * 1000).toLocaleString()
-)
+async function checkRateLimit() {
+  // https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#checking-your-rate-limit-status
+  const response = await fetch(
+    `https://api.github.com/users/${process.env.USER}`,
+    {
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    }
+  )
+
+  const limit = response.headers.get('x-ratelimit-limit')
+  const remaining = response.headers.get('x-ratelimit-remaining')
+  const used = response.headers.get('x-ratelimit-used')
+  const reset = response.headers.get('x-ratelimit-reset')
+  console.log('limit:', limit)
+  console.log('remaining:', remaining)
+  console.log('used:', used)
+  console.log(
+    'reset:',
+    new Date(parseInt(reset as string) * 1000).toLocaleString()
+  )
+}
+
+checkRateLimit()
